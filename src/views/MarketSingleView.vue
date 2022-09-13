db6762bebabba3270c55aba002c3614957551d96
0xjoshva <joshuasteed123@gmail.com>
2022-09-14 01:25:24 +0200

    fixing market

--- src/views/MarketSingleView.vue
+++ src/views/MarketSingleView.vue
@@ -2,6 +2,56 @@
   <section id="section">
     <div v-if="cryptos" class="item">
       <p>{{ cryptos.info }}</p>
+
+      <div
+        style="
+          width: 250px;
+          height: 220px;
+          background-color: #ffffff;
+          overflow: hidden;
+          box-sizing: border-box;
+          border: 1px solid #56667f;
+          border-radius: 4px;
+          text-align: right;
+          line-height: 14px;
+          block-size: 220px;
+          font-size: 12px;
+          font-feature-settings: normal;
+          text-size-adjust: 100%;
+          box-shadow: inset 0 -20px 0 0 #56667f;
+          padding: 1px;
+          padding: 0px;
+          margin: 0px;
+        "
+      >
+        <div style="height: 200px; padding: 0px; margin: 0px; width: 100%">
+          <iframe
+            src="https://widget.coinlib.io/widget?type=single_v2&theme=light&coin_id=859&pref_coin_id=1505"
+            width="250"
+            height="196px"
+            scrolling="auto"
+            marginwidth="0"
+            marginheight="0"
+            frameborder="0"
+            border="0"
+            style="border: 0; margin: 0; padding: 0; line-height: 14px"
+          ></iframe>
+        </div>
+        <div
+          style="
+            color: #ffffff;
+            line-height: 14px;
+            font-weight: 400;
+            font-size: 11px;
+            box-sizing: border-box;
+            padding: 2px 6px;
+            width: 100%;
+            font-family: Verdana, Tahoma, Arial, sans-serif;
+          "
+        >
+          
+        </div>
+      </div>
     </div>
 
     <div v-else class="spinnerboxx">
@@ -44,7 +94,7 @@ export default {
 };
 </script>
 <style scoped>
-#section{
+#section {
   height: 100vh;
   width: 100%;
 }
@@ -77,5 +127,8 @@ export default {
   width: 100%;
   height: 100vh;
   align-items: center;
+}
+.single-widget-header {
+  display: none !important;
 }
 </style>
